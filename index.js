bb99aff2ff95bc7e5651bb4250a1c041d4c9b3ce
diffsky <matthew.hadley@gmail.com>
2014-05-30 16:18:15 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var path = require('path');
+var glob = require('glob');
+var MessageFormat = require('messageformat');
+var langs = {};
+var messageFormatters = {};
+var locale, lang, split, terms, key;
+
+function dictionary(opts) {
+  opts.cwd = opts.cwd || process.cwd();
+  opts.glob = opts.glob || '**/*.json';
+
+  var files = glob.sync(opts.glob, {
+    cwd: opts.cwd
+  });
+
+  files.forEach(function(file) {
+    split = file.split('/');
+    locale = split[0];
+    lang = split[0].substring(0, 2);
+
+    langs[locale] = langs[locale] || {};
+
+    messageFormatters[locale] = messageFormatters[locale] || new MessageFormat(lang);
+
+    terms = require(path.join(opts.cwd, file));
+    split = split.splice(2);
+    key = split.concat([split.pop().slice(0, -5)]).join('/');
+
+    Object.keys(terms).forEach(function(message) {
+      langs[locale] = langs[locale] || {};
+      langs[locale][key] = langs[locale][key] || {};
+      langs[locale][key][message] = messageFormatters[locale].compile(terms[message]);
+    });
+  });
+
+  return langs;
+}
+
+module.exports = dictionary;
---
+++ test/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var path = require('path');
+var test = require('tape');
+
+test('should compile a file structure into a dictionary object', function(t) {
+  t.plan(2);
+
+  var dictionary = require('..')({
+    cwd: path.join(process.cwd(), 'test', 'lang')
+  });
+
+  t.equal(typeof dictionary['en-us'].main.greeting, 'function');
+  t.equal(typeof dictionary['fr-fr'].main.greeting, 'function');
+
+});
