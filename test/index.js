bca03242d4ef9988d26b8fc164033b7b11dda835
diffsky <matthew.hadley@gmail.com>
2014-06-02 17:45:12 -0700

    upperase regions

--- test/index.js
+++ test/index.js
@@ -4,11 +4,12 @@ var path = require('path');
 var test = require('tape');
 
 test('should compile a file structure into a dictionary object', function(t) {
-  t.plan(2);
+  t.plan(3);
 
   var dictionary = require('..')({
     cwd: path.join(process.cwd(), 'test', 'lang')
   });
-  t.equal(typeof dictionary['en-us'].main.greeting, 'function');
-  t.equal(typeof dictionary['fr-fr'].main.greeting, 'function');
+  t.equal(typeof dictionary.en.main.greeting, 'function');
+  t.equal(typeof dictionary['en-US'].main.greeting, 'function');
+  t.equal(typeof dictionary.fr.main.greeting, 'function');
 });
